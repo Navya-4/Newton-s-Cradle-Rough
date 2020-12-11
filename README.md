c1555b4b334a19fd1723e3cf54690ec9a52dc86a
Navya-4 <73339744+Navya-4@users.noreply.github.com>
2020-12-11 14:36:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Newton-s-Cradle-Rough
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ rope.js
@@ -0,0 +1,36 @@
+class Rope {
+      constructor(body1,body2,offsetX,offsetY){
+      this.offsetX=offsetX;
+      this.offsetY=offsetY;
+       var options={
+           bodyA:body1,
+           bodyB:body2,
+           //stiffness:0.04,
+           //length:10
+          pointB:{x:this.offsetX,y:this.offsetY}
+       }
+       this.rope=Constraint.create(options);
+       World.add(world,this.rope);
+  }
+
+fly () {
+
+  this.sling.bodyA = null;
+}
+
+display () {
+
+if (this.sling.bodyA){
+  
+  var pointA = this.sling.bodyA.position
+  var pointB = this.pointB
+    
+  line (pointA.x,pointA.y,pointB.x,pointB.y)
+  stroke ("white")
+
+}
+
+
+}
+
+}
